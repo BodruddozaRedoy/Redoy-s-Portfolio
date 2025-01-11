55b2ee1d35efd0fd77f55eabac76d8a9805abd87
BodruddozaRedoy <bodruddozaredoy@gmail.com>
2025-01-11 23:15:23 +0600

    added one more projects

--- app/contact/page.js
+++ app/contact/page.js
@@ -166,8 +166,9 @@ const page = () => {
       <div className="contact-page-map pb-120 rpb-90 wow fadeInUp delay-0-2s">
         <div className="container">
           <div className="our-location">
+          {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8674.405905941176!2d90.37505317420073!3d23.79089243599456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7339d102069%3A0x17737054e9b7df8a!2sEast%20Shewrapara%2C%20Dhaka!5e1!3m2!1sen!2sbd!4v1736013171728!5m2!1sen!2sbd" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe> */}
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m12!1m10!1m3!1d142190.2862584524!2d-74.01298319978558!3d40.721725351435126!2m1!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1sen!2sbd!4v1663473911885!5m2!1sen!2sbd"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8674.405905941176!2d90.37505317420073!3d23.79089243599456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c7339d102069%3A0x17737054e9b7df8a!2sEast%20Shewrapara%2C%20Dhaka!5e1!3m2!1sen!2sbd!4v1736013171728!5m2!1sen!2sbd"
               style={{ border: 0, width: "100%" }}
               allowFullScreen=""
               loading="lazy"
